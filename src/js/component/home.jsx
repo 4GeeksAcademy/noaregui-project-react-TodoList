a9d7c3a994907e50125800f66051a479b524227f
Ainhoa <150278575+noaregui@users.noreply.github.com>
2024-04-05 19:31:45 +0000

    cambios

--- src/js/component/home.jsx
+++ src/js/component/home.jsx
@@ -1,34 +1,35 @@
 import React, { useState } from "react";
 
-
-//include images into your bundle
-
 //create your first component
 const Home = () => {
 	const [ inputValue, setInputValue ] = useState("")
 	const [ todos, setTodos ] = useState([]);
+	const [todosListType, setTodosListType ] = useState()
+	
 	return (
 		<div className="container">
 			<h1>My Tasks</h1>
 			<ul>
-				<li><input 
-				type="text" 
-				onChange={e => setInputValue((e).target.value)}
-				value={inputValue}
-				onKeyDown={(e) => {
-					if (e.key === "Enter") {
-						todos.push(inputValue)
-						setInputValue("");
+				<li>
+					<input 
+						type="text" 
+						value={inputValue}
+						onChange={e => setInputValue((e).target.value.trimStart())}
+						onKeyDown={(e) => {
+							if (e.key === "Enter") {
+								todos.push(inputValue)
+								setInputValue("");
+							}
+						}}	
+						placeholder="What do you need to do">
+					</input></li>
 
-					}
-				}}	
-				placeholder="What do you need to do"></input></li>
+					{todos.map((item, index) => (
+						<div className="d-block">
+							<li className="d-flex align-items-center" key={index}>{item} <i className="fas fa-times ml-auto" ></i></li>
+						</div>
+					))}
 
-				{todos.map((item, index) => (
-				<li key={index}>{item} <i className="fas fa-times"></i>
-				</li>
-			))}
-				
 			</ul>
 			<div>{todos.length} tasks</div>
 		</div>
