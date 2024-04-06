d534cf08fcab2b6b8edd535ad38d482c0cdbd72a
Ainhoa <150278575+noaregui@users.noreply.github.com>
2024-04-06 11:29:58 +0000

    cambios

--- src/js/component/home.jsx
+++ src/js/component/home.jsx
@@ -1,40 +1,94 @@
-import React, { useState } from "react";
+import React, {useState} from "react";
 
-//create your first component
 const Home = () => {
-	const [ inputValue, setInputValue ] = useState("")
-	const [ todos, setTodos ] = useState([]);
-	const [todosListType, setTodosListType ] = useState()
-	
+	/*El input estará con un string vacío (""), necesitamos que al escribir se actualice,
+	para eso setNewTask*/
+	const [newTask, setNewTask] = useState("");
+	const[ tasks, setTasks ] = useState(["Eat breakfast", "Take a shower", "Walk the dog"]);
+
+	/*Función para que al escribir en el input se actualiza el valor inicial. Es decir,
+	de ("") a lo que escribamos*/
+	/*event hace referencia a lo que se introduce en el input*/
+	function handleInputChange(event) {
+		setNewTask(event.target.value);
+	}
+
+	function addTask() {
+		if(newTask.trim() !=="") {
+		setTasks([...tasks, newTask]);
+		setNewTask("");
+		}
+	}
+
+	function deleteTask(index){
+		const updatedTasks = tasks.filter((_, i) => i !== index);
+		setTasks(updatedTasks);
+
+	}
+
+	function moveTaskUp(index) {
+		if(index > 0) {
+			const updatedTasks = [...tasks];
+			[updatedTasks[index], updatedTasks[index - 1]] = 
+			[updatedTasks[index - 1], updatedTasks[index]]
+			setTasks(updatedTasks);
+		}
+	}
+
+	function moveTaskDown(index){
+		if(index < tasks.length - 1) {
+			const updatedTasks = [...tasks];
+			[updatedTasks[index], updatedTasks[index + 1]] = 
+			[updatedTasks[index + 1], updatedTasks[index]]
+			setTasks(updatedTasks);
+		}
+	}
+
 	return (
-		<div className="container">
-			<h1>My Tasks</h1>
-			<ul>
-				<li>
-					<input 
-						type="text" 
-						value={inputValue}
-						onChange={e => setInputValue((e).target.value.trimStart())}
-						onKeyDown={(e) => {
-							if (e.key === "Enter") {
-								todos.push(inputValue)
-								setInputValue("");
-							}
-						}}	
-						placeholder="What do you need to do">
-					</input></li>
-
-					{todos.map((item, index) => (
-						<div className="d-block">
-							<li className="d-flex align-items-center" key={index}>{item} <i className="fas fa-times ml-auto" ></i></li>
-						</div>
-					))}
-
-			</ul>
-			<div>{todos.length} tasks</div>
+	<>
+	<div className="to-do-list">
+		<h1>To Do List</h1>
+		<div>
+			<input
+				type="text"
+				placeholder="Enter a task..."
+				value={newTask}
+				onChange={handleInputChange}>
+			</input>
+			<button
+				className="add-button"
+				onClick={addTask}>
+					Add
+			</button>
 		</div>
-		
+		<ol>
+			{tasks.map((task, index) => 
+				<li key={index}>
+					<span className="text">{task}</span>
+					<button
+						className="up-button"
+						onClick={() => moveTaskUp(index)}>
+							<i class="fas fa-long-arrow-alt-up"></i>
+					</button>
+					<button
+						className="down-button"
+						onClick={() => moveTaskDown(index)}>
+							<i class="fas fa-long-arrow-alt-down"></i>
+					</button>
+					<button
+						className="delete-button"
+						onClick={() => deleteTask(index)}>
+							<i class="fas fa-times"></i>
+					</button>
+				</li>
+			)}
+		</ol>
+
+	</div>
+	
+	
+	</>
 	);
-};
+}
 
 export default Home;
